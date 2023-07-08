0f046b67234af4ff61b01b1c2a7b6e27c71835c3
Aditya Kaushik <aditya.kaushik@hpe.com>
2023-07-08 18:52:41 +0530

    bug fixes

--- src/data/data.js
+++ src/data/data.js
@@ -9,42 +9,42 @@ export const MenuList = [
   {
     name: "Dosa",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "A South Indian delicacy that features a thin, crispy crepe made from fermented rice and lentil batter. Served with a side of coconut chutney and sambar, Dosa is a versatile dish that can be enjoyed for breakfast, lunch, or dinner. Our Dosa is made to perfection, with a golden-brown texture and a delectable blend of flavors.",
     image: Dosa,
     price: 200,
   },
   {
     name: "Chola",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "A popular North Indian dish made with chickpeas cooked in a flavorful tomato-based gravy. Our Chola is prepared with a blend of aromatic spices, slow-cooked to perfection, and served with fluffy bhaturas or steamed rice. This hearty and satisfying dish is a favorite among our customers, known for its rich flavors and comforting taste.",
     image: Chola,
     price: 250,
   },
   {
     name: "Idli Sambhar",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "A traditional South Indian breakfast item consisting of soft, fluffy steamed rice cakes (Idli) served with a tangy lentil soup (Sambhar) and coconut chutney. Our Idli Sambhar is made using a secret family recipe, ensuring the perfect balance of flavors and textures. It's a wholesome and nutritious option to kickstart your day.",
     image: Idli,
     price: 300,
   },
   {
     name: "Masala Dosa",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "A mouthwatering variation of the classic Dosa, Masala Dosa is filled with a spiced potato filling. It features a crisp exterior and a flavorful interior, making it a popular choice for breakfast or as a snack. Our Masala Dosa is made with a generous filling of seasoned potatoes, accompanied by coconut chutney and sambar.",
     image: MasalaDosa,
     price: 100,
   },
   {
     name: "Paneer",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "Paneer, a fresh Indian cottage cheese, takes center stage in this delectable dish. Prepared with a combination of aromatic spices, onions, tomatoes, and creamy gravy, our Paneer dish offers a rich and creamy texture that melts in your mouth. It's a vegetarian delight that pairs perfectly with naan or rice.",
     image: Paneer,
     price: 400,
   },
   {
-    name: "Gujrati",
+    name: "Gujarati Thali",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+      "Experience the diverse flavors of Gujarat with our signature Gujarati Thali. This traditional platter features a well-balanced combination of savory dishes, including daal, kachori, dhokla, shaak (vegetable curry), kadhi, roti, rice, and a variety of chutneys and pickles. Our Gujarati Thali offers a gastronomic journey through the vibrant and authentic flavors of Gujarat.",
     image: Gujrati,
     price: 500,
   },
